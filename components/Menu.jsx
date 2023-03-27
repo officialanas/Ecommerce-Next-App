37287169089f2eb8c415b652128635d37aa363b8
Anas Ansari <itsofficial177@gmail.com>
2023-03-27 11:21:08 +0530

    new

--- components/Menu.jsx
+++ components/Menu.jsx
@@ -16,15 +16,38 @@ const subMenuData = [
   { id: 4, name: "Football shoes", doc_count: 107 },
 ];
 
-const Menu = () => {
+const Menu = ({ showCatMenu, setShowCatMenu }) => {
   return (
     <ul className="hidden md:flex items-center gap-8 font-medium text-black">
       {data.map((item) => {
         return (
           <React.Fragment key={item.id}>
             {!!item?.subMenu ? (
-              <li className="cursor-pointer flex items-center gap-2 relative">
+              <li
+                className="cursor-pointer flex items-center gap-2 relative"
+                onMouseEnter={() => setShowCatMenu(true)}
+                onMouseLeave={() => setShowCatMenu(false)}
+              >
                 {item.name}
+                <BsChevronDown size={14} />
+                {showCatMenu && (
+                  <ul className="bg-white absolute top-6 left-0 min-0-w-[250px] px-1 py-1 text-black shadow-lg">
+                    {subMenuData.map((submenu) => {
+                      return (
+                        <Link
+                          href="/"
+                          key={submenu.id}
+                          onClick={() => setShowCatMenu(false)}
+                        >
+                          <li className=" h-12 flex justify-between items-center px-3 hover:bg-black/[0.03] rounded-md">
+                            {submenu.name}
+                            <span className="opacity-50 text-sm">6</span>
+                          </li>
+                        </Link>
+                      );
+                    })}
+                  </ul>
+                )}
               </li>
             ) : (
               <li className="cursor-pointer">
